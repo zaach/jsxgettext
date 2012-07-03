ac16a4db911db7b7da717abffba7ee8fe13a5450
Zachary Carter <zack.carter@gmail.com>
2012-07-03 15:07:44 -0700

    keyword support, output/output-dir support - issues #2 + #3

--- tests/wait-messages.js
+++ tests/wait-messages.js
@@ -22,7 +22,7 @@ BrowserID.Wait = (function(){
       // L10n: test single line comment
       title:  gettext("We are sorry, this request is taking a LOOONG time."),
       // ignore this comment
-      message:  gettext("This message will go away when the request completes (hopefully soon). If you wait too long, close this window and try again."),
+      message:  _("This message will go away when the request completes (hopefully soon). If you wait too long, close this window and try again."),
       id: "slowXHR"
     }
 
