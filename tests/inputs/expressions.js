6c146631f00c9b06c19d036d76bb2ab4c1c66079
Burak Yigit Kaya <ben@byk.im>
2014-01-16 17:54:02 +0200

    Add support for `gettext.call` type of calls

--- tests/inputs/expressions.js
+++ tests/inputs/expressions.js
@@ -1,6 +1,7 @@
 var templates = {
   "pin_verification": {
       landing: 'pin_verification',
-    subject: test.gettext("Confirm email address for Persona")
+    subject: test.gettext("Confirm email address for Persona"),
+    subject2: test.gettext.call(test, "Confirm email address for Persona 2")
   }
 };
