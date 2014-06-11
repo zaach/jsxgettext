cf987dd799a84100a8b639f8eeb6398f2439af38
Burak Yigit Kaya <ben@byk.im>
2014-06-11 17:35:13 +0300

    The comments we "extract" are, well, "extracted comments"
    Fixes #59.

--- test/tests/extracted_comments.js
+++ test/tests/extracted_comments.js
@@ -7,14 +7,14 @@ var jsxgettext = require('../../lib/jsxgettext');
 var utils = require('../utils');
 
 exports['test gettext from first file'] = function (assert, cb) {
-  var inputFilename = path.join(__dirname, '..', 'inputs', 'translator_comments.js');
+  var inputFilename = path.join(__dirname, '..', 'inputs', 'extracted_comments.js');
   fs.readFile(inputFilename, 'utf8', function (err, source) {
-    var result = jsxgettext.generate({'inputs/translator_comments.js': source}, {});
+    var result = jsxgettext.generate({'inputs/extracted_comments.js': source}, {});
 
     assert.equal(typeof result, 'string', 'Result should be a string');
     assert.ok(result.length > 0, 'Result should not be empty');
 
-    var outputFilename = path.join(__dirname, '..', 'outputs', 'translator_comments.pot');
+    var outputFilename = path.join(__dirname, '..', 'outputs', 'extracted_comments.pot');
     utils.compareResultWithFile(result, outputFilename, assert, cb);
   });
 };
