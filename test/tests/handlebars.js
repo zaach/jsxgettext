771eed29f6957568135178553d3e6b9f145a6d01
Alexander Jaeger <alexander.jaeger@me.com>
2016-08-16 00:02:45 +0200

    Added support for sub-helper

--- test/tests/handlebars.js
+++ test/tests/handlebars.js
@@ -15,12 +15,16 @@ exports['test handlebars'] = function (assert, cb) {
 
     assert.equal(typeof result, 'string', 'result is a string');
     assert.ok(result.length > 1, 'result is not empty');
-    assert.equal(result.split(/msgid ".+"/).length, 6, 'exactly five strings are found');
+    assert.equal(result.split(/msgid ".+"/).length, 6, 'exactly seven strings are found');
     assert.notEqual(result.indexOf('msgid "translated text"'), -1, 'result contains the first string');
     assert.notEqual(result.indexOf('msgid "block helper"'), -1, 'result contains the second string');
     assert.notEqual(result.indexOf('msgid "helpers"'), -1, 'result contains the third string');
     assert.notEqual(result.indexOf('msgid "so let\'s test"'), -1, 'result contains the fourth string');
-    assert.notEqual(result.indexOf('msgid "for \\"quotes\\""'), -1, 'result contains the fourth string');
+    assert.notEqual(result.indexOf('msgid "for \\"quotes\\""'), -1, 'result contains the fifth string');
+    assert.notEqual(result.indexOf("{name} took {numPhotos, plural, =0 {no photos} =1 {one photo} other {#"), -1, 'result contains the sixth (1/2) string');
+    assert.notEqual(result.indexOf("photos}}"), -1, 'result contains the sixth (2/2) string');
+    assert.notEqual(result.indexOf("adsasdasdasdasdasdasdasdasdaadsfasdfadsfasdfasdfasdfasdfasdfsdasasdasdasdasd"), -1, 'result contains the seventh (1/2) string');
+    assert.notEqual(result.indexOf("adsdasdasdasd"), -1, 'result contains the seventh (2/2) string');
     cb();
   });
 };
