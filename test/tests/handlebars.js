a11d9669bb47ddaac70559a9f88a3306ec7a04fc
Stephen Bartholomew <stephenbartholomew@gmail.com>
2017-03-08 20:32:35 +0000

    Handle parameters in handlebars (#113)

--- test/tests/handlebars.js
+++ test/tests/handlebars.js
@@ -15,12 +15,13 @@ exports['test handlebars'] = function (assert, cb) {
 
     assert.equal(typeof result, 'string', 'result is a string');
     assert.ok(result.length > 1, 'result is not empty');
-    assert.equal(result.split(/msgid ".+"/).length, 6, 'exactly five strings are found');
+    assert.equal(result.split(/msgid ".+"/).length, 7, 'exactly seven strings are found');
     assert.notEqual(result.indexOf('msgid "translated text"'), -1, 'result contains the first string');
     assert.notEqual(result.indexOf('msgid "block helper"'), -1, 'result contains the second string');
     assert.notEqual(result.indexOf('msgid "helpers"'), -1, 'result contains the third string');
     assert.notEqual(result.indexOf('msgid "so let\'s test"'), -1, 'result contains the fourth string');
     assert.notEqual(result.indexOf('msgid "for \\"quotes\\""'), -1, 'result contains the fourth string');
+    assert.notEqual(result.indexOf('msgid "I would like to do %1$s too"'), -1, 'result contains the sprintf string');
     cb();
   });
 };
